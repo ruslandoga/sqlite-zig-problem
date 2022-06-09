1c6e30f53fe6162ba70e20a154e469a8f368b129
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-06-09 08:30:33 +0300

    init

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule E.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :e,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {E.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ecto_sqlite3, "~> 0.7.5"}
+    ]
+  end
+end
